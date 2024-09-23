2b0845a7813d43c80c8e9dfa55e9c8cc5577ecc6
Aleksandr <aleksdark13@gmail.com>
2024-09-23 20:00:42 +0300

    add requrements

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='crm_back_end',
+    version='1.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Aleksandr',
+    author_email='aleksdark13@gmail.com',
+    description='crm back'
+)
