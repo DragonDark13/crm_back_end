6c03b6cdf7e0c85a70870f70ce9d87295d82be1b
Aleksandr <aleksdark13@gmail.com>
2025-12-21 00:09:52 +0200

    command file

--- install_and_run.bat
+++ install_and_run.bat
@@ -1,16 +1,36 @@
-@echo off
+echo === CLONE REPOSITORY ===
+if not exist crm_back_end (
+    git clone https://github.com/DragonDark13/crm_back_end.git
+)
 
-git clone https://github.com/DragonDark13/crm_back_end.git
-cd crm_back_end
-copy .flaskenv.example .flaskenv
+cd crm_back_end || exit /b 1
 
+echo === COPY ENV FILE ===
+if not exist .flaskenv (
+	copy ..\.flaskenv .env
+)
 
-py -3.10 -m venv myenv
+echo === CREATE VENV ===
+if not exist myenv (
+    py -3.10 -m venv myenv
+)
+
+echo === ACTIVATE VENV ===
 call myenv\Scripts\activate
-python create_db_postgree.py
 
+echo === UPGRADE PIP ===
 python -m pip install --upgrade pip setuptools wheel
+
+echo === INSTALL REQUIREMENTS ===
 pip install -r requirements.txt
 
+echo === CREATE DATABASE ===
+python create_db_postgree.py
+python init_data.py
+
+echo === RUN FLASK ===
+set FLASK_APP=app.py
+set FLASK_ENV=development
 flask run
+
 pause
