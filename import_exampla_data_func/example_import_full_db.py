c0786f49d4872fc4edcc6d0cf61d60608c36f0aa
Aleksandr <aleksdark13@gmail.com>
2025-12-22 12:21:48 +0200

    add init

--- import_exampla_data_func/example_import_full_db.py
+++ import_exampla_data_func/example_import_full_db.py
@@ -3,8 +3,19 @@ from import_exampla_data_func.add_new_Product import example_import_all_product
 from import_exampla_data_func.add_new_package import example_import_all_packages
 from import_exampla_data_func.add_others_investments import example_import_all_investment
 from postgreSQLConnect import db_session
+from app import app
 
-example_import_all_product()
-example_import_all_packages()
-example_import_all_investment()
-import_categories_from_csv('../example_import_data_csv/csv_categories/categories.csv', db_session)
+
+def run_import():
+    example_import_all_product()
+    example_import_all_packages()
+    example_import_all_investment()
+    import_categories_from_csv(
+        '../example_import_data_csv/csv_categories/categories.csv',
+        db_session
+    )
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        run_import()
